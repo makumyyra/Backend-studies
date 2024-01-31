569461238026e5273580d368706d4707eb38118e
Suvi Sammakkosuo <89193160+makumyyra@users.noreply.github.com>
2024-01-31 16:54:26 +0200

    Backend studies main project (Bookstore) added

---
+++ bookstore/Bookstore/src/main/java/Store24/Bookstore/model/Book.java
@@ -0,0 +1,69 @@
+package Store24.Bookstore.model;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private int publicationYear;
+	private int isbn;
+	private double price;
+	
+	public Book(String title, String author, int publicationYear, int isbn, double price) {
+		this.title = title;
+		this.author = author;
+		this.publicationYear = publicationYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+	
+	
+	
+	
+
+}
