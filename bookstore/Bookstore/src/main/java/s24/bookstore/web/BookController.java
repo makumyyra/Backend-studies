482add31f4607149841d164a9c214b178dbf0c87
Suvi Sammakkosuo <89193160+makumyyra@users.noreply.github.com>
2024-02-04 10:34:41 +0200

    added JPA and db

--- bookstore/Bookstore/src/main/java/s24/bookstore/web/BookController.java
+++ bookstore/Bookstore/src/main/java/s24/bookstore/web/BookController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import s24.bookstore.model.BookRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -15,14 +18,29 @@ public class BookController {
 	@Autowired
 	private BookRepository repository; 
 
-	@GetMapping("index")
+	@GetMapping("/index")
 	public String indexPage(Model model) {
 		model.addAttribute("books", repository.findAll());
         return "index";
 	}
 
+	@GetMapping("/booklist")
+	public String booklistPage(Model model) {
+		model.addAttribute("books", repository.findAll());
+        return "booklist";
+	}
+	
+	/*@GetMapping("/delete")
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		System.out.println(De);
 	
 	
+		public SomeData getMethodName(@RequestParam String param) {
+		return new SomeData();
+	}*/
+	
+
+
     // @RequestMapping(value= {"/", "/studentlist"})
     // public String studentList(Model model) {	
     //     model.addAttribute("students", repository.findAll());
